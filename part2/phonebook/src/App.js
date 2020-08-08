cd3c839475ca30ce28fdba42a4f75035386795ed
zuogang.li <bestmrlee@gmail.com>
2020-08-08 23:21:07 +0800

    Finish 2.8.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleAdd = (event) => {
     event.preventDefault();
@@ -12,7 +13,7 @@ const App = () => {
     if (nameList.indexOf(newName) !== -1) {
       alert( `${newName} is already added to the phonebook.`);
     } else  if (newName  !== ''){
-      const newPerson = { name: newName };
+      const newPerson = { name: newName ,number:newNumber };
       console.log("newPerson", newPerson);
       var newPersons = [...persons];
       console.log("newPersons", newPersons);
@@ -20,14 +21,20 @@ const App = () => {
       console.log("newPersons", newPersons);
       setPersons(newPersons);
       setNewName("");
+      setNewNumber("");
     };
 
 
   };
 
-  const handleChange = (event) => {
+  const handleChangeName = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
+  };
+
+  const handleChangeNumber = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -35,7 +42,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleChangeName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleChangeNumber} />
         </div>
         <div>
           <button type="submit" onClick={handleAdd}>
@@ -45,7 +55,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person, id) => (
-        <p key={id}>{person.name}</p>
+        <p key={id}>{person.name} {person.number} </p>
       ))}
     </div>
   );
