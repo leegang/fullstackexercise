f1c29389089f2057186a70e3d28cfc594aed1049
zuogang.li <bestmrlee@gmail.com>
2020-08-10 16:45:50 +0800

    Finish 2.12.

--- part2/countryflag/src/App.js
+++ part2/countryflag/src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [filter, setFilter] = useState("");
+  const [allcounties, setAllcountries] = useState([]);
+
+  const handleInput = (event) => {
+    console.log('input',event.target.value);
+    
+    setFilter(event.target.value);
+  };
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      console.log("response", response.data);
+      setAllcountries(response.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Filter value={filter} onChange={handleInput} />
+      <Results value={allcounties} filter={filter} />
     </div>
   );
+}
+
+const Filter = (props) => {
+  return (
+    <div>
+      <form>
+        find countries{" "}
+        <input value={props.value} onChange={props.onChange}></input>
+      </form>
+    </div>
+  );
+};
+
+const Results = (props) => {
+  const result = props.value.filter(
+    p => p.name.toLowerCase().indexOf(props.filter.toLowerCase()) !== -1
+  );
+
+  console.log('result' ,result);
+  
+
+  const countryList = result.map((pi) => {
+    return <p key={pi.numericCode}> {pi.name}</p>;
+  });
+
+  if (result.length  === 1 ) {
+    return (
+      <><Countryinfo country={result[0]} /></>
+    );
+  } else if (result.length <= 10 && result.length > 1) {
+    return <>{countryList}</>;
+  };
+
+  return (
+    <>
+      {" "}
+      <p>Too many matchs,specify another filter.</p>
+    </>
+  );
+};
+
+const Countryinfo = (props) =>{
+  return(
+    <>
+        <h2 > {props.country.name}</h2>
+  <p>capital {props.country.capital}</p>
+  <p>population {props.country.population}</p>
+  <h3>languages</h3>
+  <ul>
+  {props.country.languages.map(l=><li key={l.iso639_1}>{l.name}</li>)}
+  </ul>
+        <img  src={props.country.flag}   alt={props.country.name}  width="400"/>
+      </>
+  )
 }
 
 export default App;
