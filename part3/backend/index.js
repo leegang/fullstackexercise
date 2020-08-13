4aa0a4c12044c53765b42fead0615cdb81ec22dc
zuogang.li <bestmrlee@gmail.com>
2020-08-13 16:53:22 +0800

    Finish 3.1&3.2.

---
+++ part3/backend/index.js
@@ -0,0 +1,34 @@
+import express from "express"
+
+const app = express();
+
+const persons = [{
+  id: 1,
+  name: "HTML is easy",
+  number: "2019-05-30T17:30:31.098Z",
+},
+{
+  id: 2,
+  name: "Browser can execute only Javascript",
+  number: "2019-05-30T18:39:34.091Z",
+},
+{
+  id: 3,
+  name: "GET and POST are the most important methods of HTTP protocol",
+  number: "2019-05-30T19:20:14.298Z",
+}]
+  
+app.get("/api/persons", (req,res) => {
+  res.json(persons);
+});
+
+
+app.get("/info",(req,res)=>{
+  res.send(`
+  <p>Phone book has info for ${persons.length} peoples </p>
+  <p>${new Date()}  </p>`)
+});
+
+  const PORT = 3001;
+  app.listen(PORT);
+  console.log(`Server running on port ${PORT}`);
