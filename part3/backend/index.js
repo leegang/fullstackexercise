c4b703a162226241a66db04a77b3ffebd5772d37
zuogang.li <bestmrlee@gmail.com>
2020-08-14 15:36:09 +0800

    Finish 3.7.

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,11 @@
-import express from "express"
+import express from "express";
+import morgan from 'morgan';
 
 const app = express();
 app.use(express.json());
+app.use(morgan('tiny'));
+
+
 
 
 let persons = [{
